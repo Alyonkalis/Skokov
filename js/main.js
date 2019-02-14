28c96abda417a83979cc2059159e1a06b80873f4
Alyonkalis <Alyonka_lis@mail.ru>
2019-02-14 21:04:22 +0300

    mobile-menu2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+var buttonMenu = document.querySelector('.menu-button'),        //находим кнопку
+    menu = document.querySelector('.mobile-menu'),          //находим блок меню
+    body = document.querySelector('body'), 
+    closeMenu = document.querySelector('.mobile-menu__close');   
+
+
+buttonMenu.addEventListener("click", function(){        //слушаем клик по кнопке
+    if(menu.classList.contains('open')){              //пишем условие(если у меню уже есть класс open)
+        menuBg.classList.remove('show'),
+        menu.classList.remove('open'),               //удаляем класс open
+        body.classList.remove('overflow');          //удаляем класс overflow у body
+    } else {                                        //если нет класса open
+        menu.classList.add('open'),                 //добавляем класс open 
+        body.classList.add('overflow'),
+        menuBg.classList.add('show');              //добавляем класс overflow для body
+    }
+});
+
+closeMenu.addEventListener("click", function(){      //слушаем клик по кнопке
+    if(menu.classList.contains('open')){             //пишем условие(если у меню уже есть класс open)
+        menu.classList.remove('open'),              //удаляем класс open
+        body.classList.remove('overflow'),
+        menuBg.classList.remove('show');           //удаляем класс overflow у body
+    } else {                                            //если нет класса open
+        menu.classList.add('open'),                     //добавляем класс open 
+        body.classList.add('overflow'),
+        menuBg.classList.add('show');               //добавляем класс overflow для body
+    }
+});
+
+
+
+
+
+
 //плавная прокрутка по якорям
 $(document).ready(function(){ 
     //слушаем клик по тегу а в блоке .site-navigation 
@@ -10,3 +45,4 @@ $(document).ready(function(){
 	    $('body,html').animate({scrollTop: top}, 1500); 
     });
 });
+
