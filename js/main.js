78e7ac8a9a6404ad526d669760b0a5e889437d1b
Alyonkalis <Alyonka_lis@mail.ru>
2019-02-15 10:33:47 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-var buttonMenu = document.querySelector('.menu-button'),        //находим кнопку
-    menu = document.querySelector('.mobile-menu'),          //находим блок меню
-    body = document.querySelector('body'), 
-    closeMenu = document.querySelector('.mobile-menu__close');   
-
-
-buttonMenu.addEventListener("click", function(){        //слушаем клик по кнопке
-    if(menu.classList.contains('open')){              //пишем условие(если у меню уже есть класс open)
-        menuBg.classList.remove('show'),
-        menu.classList.remove('open'),               //удаляем класс open
-        body.classList.remove('overflow');          //удаляем класс overflow у body
-    } else {                                        //если нет класса open
-        menu.classList.add('open'),
-        buttonMenu.classList.add('mobile-menu__close'),                 //добавляем класс open 
-        body.classList.add('overflow'),
-        menuBg.classList.add('show');              //добавляем класс overflow для body
-    }
-});
-
-closeMenu.addEventListener("click", function(){      //слушаем клик по кнопке
-    if(menu.classList.contains('open')){             //пишем условие(если у меню уже есть класс open)
-        menu.classList.remove('open'),              //удаляем класс open
-        body.classList.remove('overflow'),
-        menuBg.classList.remove('show');           //удаляем класс overflow у body
-    } else {                                            //если нет класса open
-        menu.classList.add('open'),                     //добавляем класс open 
-        body.classList.add('overflow'),
-        menuBg.classList.add('show');               //добавляем класс overflow для body
-    }
-});
-
-
+var body = document.querySelector('body');
 
 
 //плавная прокрутка по якорям
@@ -44,4 +13,22 @@ $(document).ready(function(){
 	    $('body,html').animate({scrollTop: top}, 1500); 
     });
 });
+
+//плавная прокрутка по якорям
+$(document).ready(function(){ 
+    //слушаем клик по тегу а в блоке .site-navigation 
+    $(".mobile-menu__list").on("click","a", function (event) { 
+	    //забираем идентификатор блока из атрибута href 
+	    var id = $(this).attr('href'), 
+	    //узнаем высоту от начала страницы до блока на который ссылается якорь 
+	    top = $(id).offset().top; 
+	    //анимируем переход на расстояние - top за 1500 мс 
+	    $('body,html').animate({scrollTop: top}, 1500); 
+    });
+});
+
+$('button').on('click', function(){
+  $('body').toggleClass('open');
+});
+
 
